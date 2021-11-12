6a94e8bfbf1a7be75a09390833a2faff1f3d8c3a
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2021-11-12 15:07:20 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -945,7 +945,7 @@ class Database:
             print(e) 
             return False
 
-    Donlee_bt = InlineKeyboardMarkup( [[ InlineKeyboardButton("DEPLOY", url="t.me/Mo_Tech_YT")]])
+    Donlee_bt = InlineKeyboardMarkup( [[ InlineKeyboardButton("DEPLOY", url="t.me/SHProSearchbot")]])
     async def delall_filters(self, group_id: int):
         """
         A Funtion To delete all filters of a group
